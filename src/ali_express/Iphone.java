6f0bf5d8c0cb47dbdfe9fa6830cfd8008c2c9124
zulfikarayub <zulfikarayub95@gmail.com>
2019-10-02 09:57:57 +0530

    code added

--- src/ali_express/Iphone.java
+++ src/ali_express/Iphone.java
@@ -1,7 +1,13 @@
 package ali_express;
 
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 import org.junit.Assert;
 import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -17,9 +23,9 @@ public class Iphone {
 		System.setProperty("webdriver.chrome.driver", exePath);
 		WebDriver driver = new ChromeDriver();
 		driver.get("https://www.aliexpress.com");
-
+		driver.manage().window().maximize();
 		try {
-			String loginpage = pageSourceValidation();
+			String loginpage = driver.getTitle();
 			String VerifyLoginMessage = "AliExpress - Online Shopping for Popular Electronics, Fashion, Home & Garden, Toys & Sports, Automobiles and More.";
 			Assert.assertEquals(VerifyLoginMessage, loginpage);
 
@@ -30,22 +36,65 @@ public class Iphone {
 		WebElement popupclose = wait.until(ExpectedConditions.visibilityOfElementLocated(
 				By.xpath("//div[contains(@class, 'ui-window-content')]//a[contains(@class, 'close-layer')]")));
 		popupclose.click();
-		// div[contains(@class, 'searchbar-operate-box')]//input
+
 		WebElement serachBar = wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("search-key")));
 		serachBar.click();
 		serachBar.clear();
 		serachBar.sendKeys("Iphone");
+		serachBar.sendKeys(Keys.ENTER);
 
-		WebElement SearchButton = wait.until(ExpectedConditions
-				.visibilityOfElementLocated(By.xpath("/div[contains(@class, 'searchbar-operate-box')]//input")));
-		SearchButton.click();
+		// after search page display
+
+		String AfterSearch = driver.getTitle();
+		String VerifySearchMessage = "Buy Iphone and get free shipping on AliExpress.com";
+		Assert.assertEquals(VerifySearchMessage, AfterSearch);
+
+		WebElement PageNavigation = driver
+				.findElement(By.xpath("//div[contains(@class, 'next-pagination-list')]//button[text()='2']"));
+		((JavascriptExecutor) driver).executeScript("arguments[0].scrollIntoView(true);", PageNavigation);
+		PageNavigation.click();
+		WebElement itemCount = wait.until(
+				ExpectedConditions.visibilityOfElementLocated(By.xpath("//ul[contains(@class, 'list-items')]//li")));
+		int count = driver.findElements(By.xpath("//ul[contains(@class, 'list-items')]//li")).size();
+		// checking in second page if page is not null
+		if (count != 0) {
+
+			WebElement ItemClick = wait.until(ExpectedConditions.visibilityOfElementLocated(By
+					.xpath("//ul[contains(@class, 'list-items')]//li[1]//div[contains(@class,'item-title-wrap')]//a")));
+			ItemClick.click();
+
+			// swtiching the control
+
+			for (String handle : driver.getWindowHandles()) {
+
+				driver.switchTo().window(handle);
+			}
+			WebElement ItemAvailable = wait.until(ExpectedConditions
+					.visibilityOfElementLocated(By.xpath("//div[contains(@class,'product-quantity-info')]//span")));
+			String noOfpiece = ItemAvailable.getText();
+			Pattern p = Pattern.compile("\\d+");
+			Matcher m = p.matcher(noOfpiece);
+			String dsd = m.toString();
+			int result = Integer.parseInt(dsd);
+
+			if (result != 0) {
+
+				System.out.println("This item is available for shipping");
+				driver.quit();
+			}
+
+			else {
+				System.out.println("This item is not available for shipping");
+				driver.quit();
+			}
+
+		}
+
+//		WebElement SearchButton = wait.until(ExpectedConditions
+//				.visibilityOfElementLocated(By.xpath("//div[contains(@class, 'next-pagination-list')]//input")));
+//		SearchButton.click();
 
 		driver.quit();
 	}
 
-	public static String pageSourceValidation() {
-		String pagesource = driver.getTitle();
-		System.out.println(pagesource);
-		return pagesource;
-	}
 }
